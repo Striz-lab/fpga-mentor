78caf72833efee0828246883981afad0f668d695
Daniel <55478231+Striz-lab@users.noreply.github.com>
2022-10-09 08:25:33 +0300

    Update display.v

--- Task 1/7_seg/display.v
+++ Task 1/7_seg/display.v
@@ -14,19 +14,19 @@ dec_to_seg dec_to_seg(.data(b), .segments(segments));
 
 endmodule
 
-module dec_to_seg(
+module hex_to_seg(
     input [3:0]data,
 
     output reg [6:0]segments
 );
 
 always @(*) begin
-    case (data)          
+    case (data)      /*ABCDEF or abcdef*/    
         4'h0: segments = 7'b1111110; 
         4'h1: segments = 7'b0110000;
         /*
 
-        *   Write logic for missing digits (2--9) here.
+        *   Write logic for missing digits (2--F) here.
         */
     endcase
 end
