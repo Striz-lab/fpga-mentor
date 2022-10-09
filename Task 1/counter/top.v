5ff3fc1bdef92f8927abb01719aa22d37448eb62
Daniel <55478231+Striz-lab@users.noreply.github.com>
2022-10-09 04:55:37 +0300

    Update top.v

--- Task 1/counter/top.v
+++ Task 1/counter/top.v
@@ -7,9 +7,10 @@ module top(
 
 wire [3:0]anodes;
 assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = ~anodes;
+assign {DS_EN5, DS_EN6, DS_N7, DS_N8} = 4'b1111;
 
 wire [6:0]segments;
-assign {DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G} = segments;
+assign {DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G} = ~segments;
 
 wire [15:0]data; /* Data bus */
 wire clk1, clk2; /* Clocks */
