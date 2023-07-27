24507663fced29710527025190e686bf040bd086
ValentinPelmeni <71216336+ValentinPelmeni@users.noreply.github.com>
2023-07-27 19:42:24 +0300

    Update top.v

--- Task 1/7_seg/top.v
+++ Task 1/7_seg/top.v
@@ -7,7 +7,7 @@ module top(
 
 wire [15:0]data = 16'h1234; /* Data to display */
 wire [3:0]anodes;
-assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = ~anodes;
+assign {DS_EN4, DS_EN3, DS_EN2, DS_EN1} = ~anodes;
 assign {DS_EN5, DS_EN6, DS_N7, DS_N8} = 4'b1111;
 
 
